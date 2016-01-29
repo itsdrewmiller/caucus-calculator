b9dac005029c61f612d3fccac733f68edaa88619
Drew Miller <drew@drewmiller.net>
2016-01-29 09:37:02 -0500

    shim doesn't support multiline strings

--- app.js
+++ app.js
@@ -3,61 +3,59 @@
 	app.CaucusCalculator =
 		ng.core.Component({
 			selector: 'caucus-calculator',
-			template: `
-				<div class="row form-group">
-					<div class="col-sm-9">
-						<h2>Precinct Results Calculator</h2>
-					</div>
-				</div>
-				<div class="row form-group">
-					<label class="col-sm-3 form-control-label" for="delegates">Total Delegates</label>
-					<div class="col-sm-3">
-						<input class="form-control" type="phone" id="delegates" [(ngModel)]="delegates" />
-					</div>
-				</div>
-				<div class="row form-group">
-					<label class="col-sm-3 form-control-label" for="attendees">All Attendees</label>
-					<div class="col-sm-3">
-						<input class="form-control" type="phone" id="attendees" [(ngModel)]="attendees" />
-					</div>
-				</div>
-				<div class="row form-group">
-					<label class="col-sm-3 form-control-label" for="clinton">Clinton Preference Group</label>
-					<div class="col-sm-3">
-						<input class="form-control" type="phone" id="clinton" [(ngModel)]="clinton" />
-					</div>
-					<div class="col-sm-3">
-						<span>{{clintonDelegates()}} Delegates earned</span>
-					</div>
-				</div>
-				<div class="row form-group">
-					<label class="col-sm-3 form-control-label" for="omalley">O&#39;Malley Preference Group</label>
-					<div class="col-sm-3">
-						<input class="form-control" type="phone" id="omalley" [(ngModel)]="omalley" />
-					</div>
-					<div class="col-sm-3">
-						<span>{{omalleyDelegates()}} Delegates earned</span>
-					</div>
-				</div>
-				<div class="row form-group">
-					<label class="col-sm-3 form-control-label" for="sanders">Sanders Preference Group</label>
-					<div class="col-sm-3" >
-						<input class="form-control" type="phone" id="sanders" [(ngModel)]="sanders" />
-					</div>
-					<div class="col-sm-3" >
-						<span>{{sandersDelegates()}} Delegates earned</span>
-					</div>
-				</div>
-				<div class="row form-group">
-					<label class="col-sm-3 form-control-label" for="uncommitted">Uncommitted Preference Group</label>
-					<div class="col-sm-3">
-						<span>{{uncommitted()}}</span>
-					</div>
-					<div>
-						<span class="col-sm-3">{{uncommittedDelegates()}} Delegates earned</span>
-					</div>
-				</div>
-			`
+			template: '<div class="row form-group">' +
+					'<div class="col-sm-9">' + 
+						'<h2>Precinct Results Calculator</h2>' +
+					'</div>' +
+				'</div>' +
+				'<div class="row form-group">' +
+					'<label class="col-sm-3 form-control-label" for="delegates">Total Delegates</label>' +
+					'<div class="col-sm-3">' +
+						'<input class="form-control" type="phone" id="delegates" [(ngModel)]="delegates" />' +
+					'</div>' +
+				'</div>' +
+				'<div class="row form-group">' +
+				'	<label class="col-sm-3 form-control-label" for="attendees">All Attendees</label>' +
+				'	<div class="col-sm-3">' +
+				'		<input class="form-control" type="phone" id="attendees" [(ngModel)]="attendees" />' +
+				'	</div>' +
+				'</div>' +
+				'<div class="row form-group">' +
+				'	<label class="col-sm-3 form-control-label" for="clinton">Clinton Preference Group</label>' +
+				'	<div class="col-sm-3">' +
+				'		<input class="form-control" type="phone" id="clinton" [(ngModel)]="clinton" />' +
+				'	</div>' +
+				'	<div class="col-sm-3">' +
+				'		<span>{{clintonDelegates()}} Delegates earned</span>' +
+				'	</div>' +
+				'</div>' +
+				'<div class="row form-group">' +
+				'	<label class="col-sm-3 form-control-label" for="omalley">O&#39;Malley Preference Group</label>' +
+				'	<div class="col-sm-3">' +
+				'		<input class="form-control" type="phone" id="omalley" [(ngModel)]="omalley" />' +
+				'	</div>' +
+				'	<div class="col-sm-3">' +
+				'		<span>{{omalleyDelegates()}} Delegates earned</span>' +
+				'	</div>' +
+				'</div>' +
+				'<div class="row form-group">' +
+				'	<label class="col-sm-3 form-control-label" for="sanders">Sanders Preference Group</label>' +
+				'	<div class="col-sm-3" >' +
+				'		<input class="form-control" type="phone" id="sanders" [(ngModel)]="sanders" />' +
+				'	</div>' +
+				'	<div class="col-sm-3" >' +
+				'		<span>{{sandersDelegates()}} Delegates earned</span>' +
+				'	</div>' +
+				'</div>' +
+				'<div class="row form-group">' +
+				'	<label class="col-sm-3 form-control-label" for="uncommitted">Uncommitted Preference Group</label>' +
+				'	<div class="col-sm-3">' +
+				'		<span>{{uncommitted()}}</span>' +
+				'	</div>' +
+				'	<div>' +
+				'		<span class="col-sm-3">{{uncommittedDelegates()}} Delegates earned</span>' +
+				'	</div>' +
+				'</div>'
 		})
 		.Class({
 			constructor: function() {
